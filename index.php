f775bdb1185895b81308e557baead054b05e5983
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-08 12:22:58 +0200

    construct

--- index.php
+++ index.php
@@ -6,9 +6,16 @@ class Movies {
     public $genre;
     public $director;
     public $length;
-    public $overview;
+    public $year;
+
+    public function __construct(string $_title, string $_genre, string $_director, string $_length, int $_year) {
+        $this -> title = $_title;
+        $this -> genre = $_genre;
+        $this -> director = $_director;
+        $this -> length = $_length;
+        $this -> year = $_year; 
+    }
 };
 
-$movie = new Movies();
-$movie -> title = 'Il signore degli anelli - La compagnia dell\' anello';
+$movie = new Movies('il signore degli anelli - la compagnia dell\'anello', 'Fantasy', 'Peter Jackson', '178 minuti', 2001);
 var_dump($movie);
