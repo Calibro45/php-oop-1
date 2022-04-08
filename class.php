40225182622807e9a94989ce3a3ff8c7404714a4
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-04-08 14:02:28 +0200

    stampa titoli

--- class.php
+++ class.php
@@ -2,29 +2,57 @@
 
 class Movies {
 
-    public $title;
-    public $genre;
-    public $director;
-    public $length;
-    public $year;
+    private $title;
+    private $genre;
+    private $director;
+    private $length;
+    private $year;
 
-    public function __construct(string $_title, string $_genre, string $_director, string $_length, int $_year) {
+    public function __construct(string $_title, string $_genre, string $_director, string $_length) {
         $this -> title = $_title;
         $this -> genre = $_genre;
         $this -> director = $_director;
         $this -> length = $_length;
-        $this -> year = $_year; 
+    }
+
+    public function setYear($_year) {
+        if(is_numeric($_year)) {
+            $this -> year = $_year;
+        }
+    }
+
+    public function getTitle() {
+        return $this -> title; 
+    }
+
+    public function getGenre() {
+        return $this -> genre; 
+    }
+
+    public function getDirector() {
+        return $this -> director; 
+    }
+
+    public function getLength() {
+        return $this -> length; 
+    }
+
+    public function getYear() {
+        return $this -> year; 
     }
 
 };
 
-$movie = new Movies('Il signore degli anelli - la compagnia dell\'anello', 'Fantasy', 'Peter Jackson', '178 minuti', 2001);
+$movie1 = new Movies('Il signore degli anelli - la compagnia dell\'anello', 'Fantasy', 'Peter Jackson', '178 minuti');
+$movie1 -> setYear(2001);
 //var_dump($movie);
 
-$movie2 = new Movies('Il signore degli anelli - Le due torri', 'Fantasy', 'Peter Jackson', '179 minuti', 2002);
+$movie2 = new Movies('Il signore degli anelli - Le due torri', 'Fantasy', 'Peter Jackson', '179 minuti');
+$movie2 -> setYear(2002);
 //var_dump($movie2);
 
-$movie3 = new Movies('Il signore delgi anelli - Il ritorno del Re', 'Fantasy', 'Peter Jackson', '200 minuti', 2003);
+$movie3 = new Movies('Il signore delgi anelli - Il ritorno del Re', 'Fantasy', 'Peter Jackson', '200 minuti');
+$movie3 -> setYear(2003);
 //var_dump($movie3);
 
 function pushMovies($movie_1, $movie_2, $movie_3) {
@@ -33,5 +61,5 @@ function pushMovies($movie_1, $movie_2, $movie_3) {
     return $movieCatalog;
 };
 
-$movies = pushMovies($movie, $movie2, $movie3);
-var_dump($movies);
+$moviesCat = pushMovies($movie1, $movie2, $movie3);
+//var_dump($moviesCat);
--- index.php
+++ index.php
@@ -3,3 +3,39 @@
 require_once __DIR__ . '/class.php'; 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>OOP-php</title>
+</head>
+<body>
+    <main>
+        <div class="container">
+            <ol>
+                <?php 
+                
+                foreach($moviesCat as $movie ):
+                    $title = $movie -> getTitle();
+                    $genre = $movie -> getGenre();
+                    $director = $movie -> getDirector();
+                    $length = $movie -> getLength();
+                    $year = $movie -> getYear();
+
+                    ?> 
+                    <li>
+                        <h4><?php echo $title ?></h4>
+                    </li>
+                    <?php
+                endforeach
+                
+                ?>
+
+            </ol>
+        </div>
+    </main>
+</body>
+</html>
